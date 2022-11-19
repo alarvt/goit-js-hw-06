4d16ffeda2340be98defec185e9bca0bee9ea9a3
alarvt <alarvt@gmail.com>
2022-11-19 14:13:33 +0100

    ДЗ-2

--- js/task-08.js
+++ js/task-08.js
@@ -12,6 +12,31 @@ function handleSubmit(event) {
     return console.log("Please fill in all the fields!");
   }
 
-  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  const outputForm = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(outputForm);
+
+  /* console.log(`Login: ${email.value}, Password: ${password.value}`); */
   event.currentTarget.reset();
 }
+
+/* const formEl = document.querySelector(".login-form");
+formEl.addEventListener("submit", fieldsCheck);
+function fieldsCheck(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+  if (email.length === 0 || password.length === 0) {
+    alert("Заповнені не всі поля! Доповніть, інформацію, будь-ласка");
+    return;
+  }
+  const formData = {
+    email,
+    password,
+  };
+  console.log(formData);
+  formEl.reset(); */
