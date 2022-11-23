67aa87235cfc11e77a5ad828bd5de3305727a4aa
alarvt <alarvt@gmail.com>
2022-11-23 11:13:08 +0100

    ДЗ-7

--- js/task-05.js
+++ js/task-05.js
@@ -2,25 +2,12 @@
 const inputName = document.querySelector('#name-input');
 const outputName = document.querySelector("#name-output");
 
-/* inputName.addEventListener("input", (event) => {
-  outputName.textContent = event.currentTarget.value;
-});
-
-function onInputChange(event) {
-  refs.output.textContent = event.currentTarget.value;
-  if (!refs.output.textContent) {
-    return (refs.output.textContent = 'Anonymous');
-  }
-} */
-
-
-
 inputName.addEventListener("input", onInputChange);
 
 function onInputChange(event) {
-  refs.output.textContent = event.currentTarget.value;
-  if (!refs.output.textContent) {
-    refs.output.textContent = "Anonymous";
+  outputName.textContent = event.currentTarget.value;
+  if (!outputName.textContent) {
+    outputName.textContent = "Anonymous";
   }
 } 
 
